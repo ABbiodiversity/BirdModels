457c9724c4484b2933bc69c7642655f7a09ca276
ecknight <ellyknight@gmail.com>
2022-11-16 12:12:02 -0700

    Initial commit

---
+++ script/01.GetData.R
@@ -0,0 +1,58 @@
+# ---
+# title: "QPAD estimation - get data"
+# author: "Elly Knight"
+# created: "July 24, 2022"
+# ---
+
+library(tidyverse)
+library(wildRtrax)
+library(data.table)
+
+#1. Get list of projects from WildTrax----
+wt_auth()
+
+#sensor = PC gives all ARU and point count projects
+project.list <- wt_get_download_summary(sensor_id = 'PC')
+
+#2. Convert to a plain dataframe----
+projects <- data.frame(project = as.character(project.list$project),
+                       project_id = as.numeric(project.list$project_id),
+                       sensorId = as.character(project.list$sensorId),
+                       tasks = as.numeric(project.list$tasks),
+                       status = as.character(project.list$status))
+
+#3. Loop through projects to download data----
+dat.list <- list()
+for(i in 1:nrow(projects)){
+  
+  if(projects$sensorId[i]=="ARU"){
+    
+    #Get summary report
+    report.try <- try(wt_download_report(project_id = projects$project_id[i], sensor_id = projects$sensorId[i], weather_cols = F, report = "summary"))
+    
+    #Get task report for ARU model
+    task.try <- try(wt_download_report(project_id = projects$project_id[i], sensor_id = projects$sensorId[i], weather_cols = F, report = "task"))
+    
+    if(class(report.try)=="data.frame"){
+      dat.try <- report.try %>% 
+        left_join(task.try)
+    }
+  }
+  
+  if(projects$sensorId[i]=="PC"){
+    dat.try <- try(wt_download_report(project_id = projects$project_id[i], sensor_id = projects$sensorId[i], weather_cols = F, report="report"))
+  }
+  
+  if(class(dat.try)=="data.frame"){
+    dat.list[[i]] <- dat.try
+  }
+  
+  print(paste0("Finished dataset ", projects$project[i], " : ", i, " of ", nrow(projects), " projects"))
+  
+}
+
+#4. Collapse list----
+raw <- rbindlist(dat.list, fill=TRUE)
+
+#5. Save date stamped data & project list----
+save(raw, projects, file=paste0("data/wildtrax_data_", Sys.Date(), ".Rdata"))
---
+++ script/02.WrangleData.R
@@ -0,0 +1,76 @@
+library(tidyverse) #basic data wrangling
+library(downloader) #download files
+library(sf) #for spatial wrangling
+library(terra) #for spatial wrangling
+
+#Load previous dataset----
+load("data/ab-birds-all-2022-03-09.RData")
+
+#1. Load data downloaded from WildTrax----
+load("data/wildtrax_data_2022-11-15.RData")
+
+#2. Wrangle----
+#tidy column names between sensor types
+#tidy method data
+#add sensor type
+dat <- raw  %>% 
+  data.frame() %>% 
+  # mutate(project = ifelse(is.na(project), project_name, project),
+  #        speciesCode = ifelse(is.na(speciesCode), species_code, speciesCode), 
+  #        date = ifelse(is.na(date), recording_date, date)) %>% 
+  # dplyr::select(-project_name, -species_code, -recording_date) %>% 
+  separate(method, into=c("duration", "method"), remove=TRUE) %>% 
+  mutate(duration = ifelse(!method %in% c("1SPM", "1SPT", "None"), method, duration),
+         method = ifelse(!method %in% c("1SPM", "1SPT", "None"), "None", method)) %>% 
+  left_join(projects %>% 
+              rename(sensor = sensorId) %>% 
+              dplyr::select(project, sensor))
+
+#3. Remove unstructured survey data----
+dat.method <- dat %>% 
+  mutate(durationMethod = ifelse(is.na(durationMethod), "ARU", durationMethod)) %>% 
+  dplyr::filter(durationMethod!="UNKNOWN",
+                !(method=="None" & sensor=="ARU"))
+
+#4. Identify unique survey locations----
+visit <- dat.method %>% 
+  dplyr::select(organization, project, location, latitude, longitude, buffer) %>% 
+  unique() %>% 
+  dplyr::filter(!is.na(latitude))
+
+#5. Remove data outside of alberta----
+
+#5a. Get provincial shapefile----
+# temp <- tempfile()
+# download("https://extranet.gov.ab.ca/srd/geodiscover/srd_pub/boundaries/census/Alberta_Census_Boundaries_SHP.zip", temp)
+# unzip(zipfile=temp, exdir="data/ABshp")
+# unlink(temp)
+
+#5b.Read in & wrangle shapefile----
+ab <- read_sf("data/ABshp/Data/AB_CD_2021.shp") %>% 
+  st_union() %>% 
+  st_transform(crs=4326) %>% 
+  st_as_sf() %>% 
+  mutate(prov = "AB") 
+
+#5c. Make survey data spatial----
+visit.sf <- visit %>% 
+  st_as_sf(coords=c("longitude", "latitude"), crs=4326)
+
+#5d. Intersect with shapefile----
+visit.ab <- visit.sf %>% 
+  st_intersection(ab)
+
+#6. Identify buffered points & tidy to save----
+#remove buffered points that aren't ABMI
+visit.use <- visit.ab %>% 
+  st_coordinates() %>% 
+  data.frame() %>% 
+  data.frame(visit.ab) %>% 
+  dplyr::select(-geometry) %>% 
+  mutate(buffer = ifelse(buffer=="0m (Buffered location)", NA, buffer)) %>% 
+  mutate(topsecret = ifelse((!is.na(buffer) & organization=="ABMI"), 1, 0)) %>% 
+  dplyr::filter(!(!is.na(buffer) & topsecret!=1))
+
+#7. Save for GIS extraction----
+write.csv(visit.use, "data/birds_ab_locations.csv", row.names = FALSE)
