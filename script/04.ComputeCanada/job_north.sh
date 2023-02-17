220df5dde2f6de4fed2a715f14968cccd30a1266
ecknight <ellyknight@gmail.com>
2023-02-17 16:43:44 -0700

    rewrite from the terminal to change line break formatting to LINUX

--- script/04.ComputeCanada/job_north.sh
+++ script/04.ComputeCanada/job_north.sh
@@ -1,25 +1,13 @@
 #!/bin/bash
-#SBATCH --account=def-psolymos  # replace this with your own account
-#SBATCH --nodes=2               # number of whole nodes
-#SBATCH --ntasks-per-node=32    # 32 cores on each node
-#SBATCH --mem=0                 # use all ~3.9G mem per core
-#SBATCH --time=12:00:00         # time (HH:MM:SS)
+#SBATCH --account=def-bayne
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=32
+#SBATCH --mem=0
+#SBATCH --time=18:00:00
 #SBATCH --job-name=north
 #SBATCH --output=%x-%j.out
-#SBATCH --mail-user=solymos@ualberta.ca
-#SBATCH --mail-type=ALL
-
-# Load modules
-module load nixpkgs/16.09
-module load gcc/7.3.0
-module load openmpi/3.1.2
-module load r/3.5.1
-
-
-# Export the nodes names.
-# If all processes are allocated on the same node,
-# NODESLIST contains : node1 node1 node1 node1
+#SBATCH --mail-user=ecknight@ualberta.ca
+module load StdEnv/2020
+module load r/4.2.1
 export NODESLIST=$(echo $(srun hostname))
-
-# Run R script
-Rscript --vanilla north.R
+Rscript --vanilla 04A.north.R
